fc4b5254183bd60209946fdd218a6b41b54529ea
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-05 09:23:38 -0300

    learn(ap01-mss); lida com o codigo 4.3.1

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+//API: uma coleção de endpoints
+//um endpoint é caracterizado por
+//um método do protocolo http
+// um padrão de acesso
+//uma funcionalidade
+//GET /lembretes () => {}
+app.get('/lembretes', (req, res) => {})
